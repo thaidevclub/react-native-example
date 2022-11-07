29cb7155c6d5f7ddb861806d91f3a0928be7d027
Sanon Kaewobchoey <thaidev.club@gmail.com>
2022-11-07 19:00:46 +0700

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,58 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/screens/HomeScreen';
-import SanonScreen from './src/screens/SanonScreen';
-
-const Stack = createNativeStackNavigator();
+import React, { useState } from 'react';
+import { 
+  StyleSheet, 
+  SafeAreaView, 
+  View, 
+  Text,
+  TouchableOpacity
+} from 'react-native';
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const clickHandler = () => {
+    setCounter(counter + 1);
+  };
+  
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Sanon" component={SanonScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>{counter}</Text>
+      <TouchableOpacity onPress={ clickHandler } style={styles.fab}>
+        <Text style={styles.fabIcon}>+</Text>
+      </TouchableOpacity>    
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'lightgray',
+    },
+    text: {
+      fontSize: 32,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      color: '#03A9F4',
+    },
+    fab: { 
+      position: 'absolute', 
+      width: 56, 
+      height: 56, 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      right: 20, 
+      bottom: 20, 
+      backgroundColor: '#03A9F4', 
+      borderRadius: 30, 
+      elevation: 8 
+    }, 
+    fabIcon: { 
+      fontSize: 40, 
+      color: 'white' 
+    }
+});
 
 export default App;
